8b3d8962264eb9854e634b612de2d3d87e5106cd
gpenessot <gael.penessot@gmail.com>
2024-03-06 23:31:38 +0100

    clean code

--- notion_blog_to_html.py
+++ notion_blog_to_html.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
-import sys
 import configparser
-from notion_client import Client
+import sys
 from datetime import datetime as dt
+
 from jinja2 import Environment, FileSystemLoader
+from notion_client import Client
 
-config=configparser.ConfigParser()
-config.read('config.cfg')
+config = configparser.ConfigParser()
+config.read("config.cfg")
 
-NOTION_TOKEN = config['NOTION']['TOKEN']
-DATABASE_ID = config['NOTION']['DATABASE_ID']
+NOTION_TOKEN = config["NOTION"]["TOKEN"]
+DATABASE_ID = config["NOTION"]["DATABASE_ID"]
+
 
 def read_text(client, page_id):
     """
@@ -28,10 +30,11 @@ def read_text(client, page_id):
     """
     try:
         response = client.blocks.children.list(block_id=page_id)
-        return response['results']
+        return response["results"]
     except Exception as e:
         print(f"Error occurred while making a request to the client: {e}")
         return []
+
 
 def read_database(client, database_id):
     """
@@ -47,13 +50,14 @@ def read_database(client, database_id):
     try:
         response = client.databases.query(database_id=database_id)
         # Extract the 'results' field from the API response
-        results = response['results']
+        results = response["results"]
         return results
     except Exception as e:
         print(f"An error occurred while querying the database: {e}")
         return []
 
-def get_info_from_database(client, notion_database_id, article_id:int=0):
+
+def get_info_from_database(client, notion_database_id, article_id: int = 0):
     """
     Retrieves information from a Notion database and formats it into a list of HTML content.
 
@@ -70,46 +74,58 @@ def get_info_from_database(client, notion_database_id, article_id:int=0):
             - description (str): The description of the article.
             - html_content_list (list): A list of HTML-formatted content blocks from the article.
     """
-    article_id=int(article_id)
+    article_id = int(article_id)
     database_content = read_database(client, notion_database_id)
-    url = database_content[article_id]['properties']['Illustration']['url']
-    article_page_id = database_content[article_id]['properties']['URL']['url'].split('-')[-1]
-    date = database_content[article_id]['properties']['Date']['date']['start']
-    keywords = [i['name'] for i in database_content[article_id]['properties']['Étiquettes']['multi_select']]
-    titre_article = database_content[article_id]['properties']['Nom']['title'][0]['text']['content']
-    description = database_content[article_id]['properties']['Description']['rich_text'][0]['text']['content']
-    
+    url = database_content[article_id]["properties"]["Illustration"]["url"]
+    article_page_id = database_content[article_id]["properties"]["URL"]["url"].split(
+        "-"
+    )[-1]
+    date = database_content[article_id]["properties"]["Date"]["date"]["start"]
+    keywords = [
+        i["name"]
+        for i in database_content[article_id]["properties"]["Étiquettes"][
+            "multi_select"
+        ]
+    ]
+    titre_article = database_content[article_id]["properties"]["Nom"]["title"][0][
+        "text"
+    ]["content"]
+    description = database_content[article_id]["properties"]["Description"][
+        "rich_text"
+    ][0]["text"]["content"]
+
     page_content = read_text(client, article_page_id)
     html_content_list = []
     for block in page_content:
-        block_type = block['type']
-        if block_type == 'heading_1':
-            content = block[block_type]['rich_text'][0]['text']['content']
+        block_type = block["type"]
+        if block_type == "heading_1":
+            content = block[block_type]["rich_text"][0]["text"]["content"]
             html = f"<h4>{content}</h4>"
             html_content_list.append(html)
-        elif block_type == 'heading_2':
-            content = block[block_type]['rich_text'][0]['text']['content']
+        elif block_type == "heading_2":
+            content = block[block_type]["rich_text"][0]["text"]["content"]
             html = f"<h5>{content}</h5>"
             html_content_list.append(html)
-        elif block_type == 'paragraph':
-            content = block[block_type]['rich_text'][0]['text']['content']
+        elif block_type == "paragraph":
+            content = block[block_type]["rich_text"][0]["text"]["content"]
             html = f"<p class='mt-3 text-muted'>{content}<p>"
             html_content_list.append(html)
-        elif block_type == 'code':
-            content = block[block_type]['rich_text'][0]['text']['content']
-            language = block[block_type]['language']
+        elif block_type == "code":
+            content = block[block_type]["rich_text"][0]["text"]["content"]
+            language = block[block_type]["language"]
             html = f"<pre><code class='{language}'>{content}</code></pre>"
             html_content_list.append(html)
-        elif block_type == 'image':
-            content = block[block_type]['file']['url']
+        elif block_type == "image":
+            content = block[block_type]["file"]["url"]
             html = f"<img src='{content}' alt='image'/>"
             html_content_list.append(html)
         else:
-            print('Format non pris en charge', block_type)
+            print("Format non pris en charge", block_type)
 
     return date, url, keywords, titre_article, description, html_content_list
 
-def main(article_id:int=sys.argv[1]):
+
+def main(article_id: int = sys.argv[1]):
     """
     Connects to a Notion database and retrieves information to render a blog template.
 
@@ -117,7 +133,7 @@ def main(article_id:int=sys.argv[1]):
     - NOTION_TOKEN: The authentication token required to connect to the Notion database.
     - DATABASE_ID: The ID of the Notion database from which the information will be retrieved.
     - blog_template.html: The blog template to fill with notion blog content
-    
+
     Outputs:
     - blog.html: A file containing the rendered template with the retrieved information.
 
@@ -135,14 +151,23 @@ def main(article_id:int=sys.argv[1]):
     """
 
     client = Client(auth=NOTION_TOKEN)
-    date, url, keywords, titre_article, description, html_content_list = get_info_from_database(client=client, notion_database_id=DATABASE_ID, article_id=article_id)
+    (
+        date,
+        url,
+        keywords,
+        titre_article,
+        description,
+        html_content_list,
+    ) = get_info_from_database(
+        client=client, notion_database_id=DATABASE_ID, article_id=article_id
+    )
     results_filename = f"blog-{titre_article.replace(' ', '_')}.html"
     environment = Environment(loader=FileSystemLoader("."))
     results_template = environment.get_template("blog_template.html")
     context = {
         "description": description,
-        "keywords": ' '.join(keywords),
-        "url":url,
+        "keywords": " ".join(keywords),
+        "url": url,
         "titre_article": titre_article,
         "sous_titre_article": html_content_list[0],
         "content": html_content_list[1:],
@@ -155,5 +180,6 @@ def main(article_id:int=sys.argv[1]):
         results.write(results_template.render(context))
         print(f"... wrote {results_filename}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- notion_to_text.py
+++ notion_to_text.py
@@ -1,80 +1,81 @@
-import requests
+import configparser
 import json
 import os
-from notion_client import Client
 import pprint
-import configparser
 
-config=configparser.ConfigParser()
-config.read('config.cfg')
+import requests
+from notion_client import Client
 
-#https://youtube.com/shorts/O6qwIEmFRmw?feature=shared
-NOTION_TOKEN = config['NOTION']['TOKEN']
-PAGE_ID =  config['NOTION']['PAGE_ID']
+config = configparser.ConfigParser()
+config.read("config.cfg")
+
+NOTION_TOKEN = config["NOTION"]["TOKEN"]
+PAGE_ID = config["NOTION"]["PAGE_ID"]
+
 
 def write_text(client, page_id, text):
     client.blocks.children.append(
         block_id=page_id,
         children=[
             {
-                "object":"block",
-                "type":"paragraph",
-                "paragraph":{
-                    "rich_text": [
-                        {
-                            "type":"text",
-                            "text":
-                                {"content": text}
-                        }
-                    ]
-                }
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [{"type": "text", "text": {"content": text}}]
+                },
             }
-        ]
+        ],
     )
 
 
 def write_dict_to_json(content, file_name):
     content_as_json_str = json.dumps(content)
-    with open(file_name, 'w') as f:
+    with open(file_name, "w") as f:
         f.write(content_as_json_str)
 
+
 def read_text(client, page_id):
-    response = client.blocks.children.list(block_id=PAGE_ID)
-    #write_dict_to_json(response, "response.json")
-    return response['results']
-    
+    response = client.blocks.children.list(block_id=page_id)
+    # write_dict_to_json(response, "response.json")
+    return response["results"]
+
+
 def create_simple_blocks_from_content(client, content):
     page_simple_blocks = []
     for block in content:
-        block_id = block['id']
-        block_type = block['type']
-        has_children = block['has_children']
-        
+        block_id = block["id"]
+        block_type = block["type"]
+        has_children = block["has_children"]
+
         if block_type not in ["code", "image"]:
-            rich_text = block[block_type]['rich_text']
+            rich_text = block[block_type]["rich_text"]
             simple_block = {
-                'id': block_id,
-                'type': block_type,
-                'text': rich_text[0]['plain_text'],
+                "id": block_id,
+                "type": block_type,
+                "text": rich_text[0]["plain_text"],
             }
-            
+
             if has_children:
                 nested_children = read_text(client, block_id)
-                simple_block['children']= create_simple_blocks_from_content(client, nested_children)
-       
-            #if not rich_text:
+                simple_block["children"] = create_simple_blocks_from_content(
+                    client, nested_children
+                )
+
+            # if not rich_text:
             #    return
-     
+
             page_simple_blocks.append(simple_block)
-    
+
     return page_simple_blocks
-        
+
+
 def main():
     client = Client(auth=NOTION_TOKEN)
     content = read_text(client, PAGE_ID)
-    write_dict_to_json(content, 'content.json')
+    write_dict_to_json(content, "content.json")
     simple_blocks = create_simple_blocks_from_content(client, content)
-    write_dict_to_json(simple_blocks, 'simple_blocks.json')
-    
-if __name__ == '__main__':
+    write_dict_to_json(simple_blocks, "simple_blocks.json")
+
+
+if __name__ == "__main__":
     main()
